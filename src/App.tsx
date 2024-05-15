c6847e5ee9d7d748ad3ab358624982a8a525f397
marjunstudio <uebujie@gmail.com>
2024-05-16 00:09:06 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './api';
+
+const HelloButton: React.FC = () => {
+  const handleClick = async () => {
+    try {
+      const response = await api.get('/');
+      const data = response.data;
+      alert(data.message);
+    } catch (error) {
+      console.error('Error:', error);
+      alert('取得に失敗しました');
+    }
+  };
+
+  return (
+    <button className="bg-red-200" onClick={handleClick}>
+      ボタン
+    </button>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloButton />
     </div>
   );
 }
